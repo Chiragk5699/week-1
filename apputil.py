bcc40c139a4a72723635104b772bc1c09325ee43
Yamini Hariharan <45371585+YaminiHariharan@users.noreply.github.com>
2026-01-21 21:32:05 +0000

    initial commit

--- apputil.py
+++ apputil.py
@@ -1,3 +1,18 @@
 
 
-# add code below ...
+# Exercise 1
+import numpy as np
+
+
+def palindrome(word):
+    word = word.replace(" ", "").lower()
+    
+    #Check if the string is the same forwards and backwards
+    return word == word[::-1]
+
+word = input("Enter a word or phrase: ")
+
+if palindrome(word):
+    print("It's a palindrome!")
+else:
+    print("Not a palindrome.")
