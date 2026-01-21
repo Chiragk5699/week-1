59327e039e5f2cc9a1955de7a4f9385cf13f3d19
Chiragk5699 <ckarachi@iu.edu>
2026-01-21 20:51:05 +0000

    We did great

--- apputil.py
+++ apputil.py
@@ -1,3 +1,23 @@
 
 
-# add code below ...
+import numpy as np
+
+
+def palindrome(word) :
+    '''
+    Given a word, return True if the word is a palindrome
+    Flase Otherwise
+    '''
+
+    word = word.lower()replace
+
+
+    return word == word[::-1]
+
+
+def parentheses(sequence):
+
+
+    return None
+
+    
